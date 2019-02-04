ac934233aae03d8a5529d62c91f3fffdf30bf4f7
pavikabilan <47311025+pavikabilan@users.noreply.github.com>
2019-02-04 22:40:57 +0530

    Update vowels.py

--- vowels.py
+++ vowels.py
@@ -3,7 +3,7 @@ x=input()
 if (x=='a' or x=='e' or x=='i' or x=='o' or x=='u') or (x=='A' or x=='E' or x=='I' or x=='O' or x=='U'):
 	print("vowels")
 elif (x>='a' and x<='z') or (x>='A' and x<='Z'):
-	print("consonants")
+	print("consonant")
 else:
 	print("invalid")
 	
