c2860a93ecfc844f314d6650d45d39a8a8d2a191
pavikabilan <47311025+pavikabilan@users.noreply.github.com>
2019-03-25 21:02:38 +0530

    Create evenodd.py

---
+++ evenodd.py
@@ -0,0 +1,6 @@
+#pavithra
+x=int(input("enter the number"))
+if x%2==0:
+    print("the number is even")
+else:
+    print("invalid")                                                                          
