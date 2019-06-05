90c4c0053fb5e5852c9456cc79ef98022f8ef06f
pavikabilan <47311025+pavikabilan@users.noreply.github.com>
2019-06-05 22:24:20 +0530

    Create pro25.py

---
+++ pro25.py
@@ -0,0 +1,10 @@
+#pavithra
+n=int(input())
+l=list(map(int,input().split()))
+m1=[]
+for i in l:
+	s=bin(i)[2::]
+	m1.append(([s.count("1"),i]))
+m1.sort(reverse=True)
+for i in range(0,len(m1)):
+	print(m1[i][1])
