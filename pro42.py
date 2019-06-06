df7b8c26fb2e6757661fe9375901ed62e78524f9
pavikabilan <47311025+pavikabilan@users.noreply.github.com>
2019-06-05 20:33:56 -0700

    Create pro42.py

---
+++ pro42.py
@@ -0,0 +1,16 @@
+# pavithra
+a5,b5=map(int,input().split())
+if a5<=b5:
+  d=a5
+else:
+  d=b5
+z=[]
+for i in range(0,d):
+  z.append(sorted(list(map(int,input().split()))))
+z=sorted(z)
+for i in range(0,len(z[0])):
+  for j in range(0,len(z)-1):
+    if z[j][i]>z[j+1][i]:
+      z[j][i],z[j+1][i]=z[j+1][i],z[j][i]
+for i in z:
+  print(*i)
