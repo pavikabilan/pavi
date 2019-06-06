d6756dd71d64089b890a4d9ac025e97f4d58ba55
pavikabilan <47311025+pavikabilan@users.noreply.github.com>
2019-06-05 21:53:01 -0700

    Create pro444.py

---
+++ pro444.py
@@ -0,0 +1,10 @@
+#pavi
+n,p,q,r=map(int,input().split())
+l=list(map(int,input().split()))
+c=[]
+for i in range(0,len(l)):
+	for j in range(i,len(l)):
+		for k in range(j,len(l)):
+			c.append(p*l[i]+q*l[j]+r*l[k])
+print(max(c))
+#max
