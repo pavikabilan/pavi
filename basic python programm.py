1454680924ae59fb9cf052312b5107600e3d7c83
pavikabilan <47311025+pavikabilan@users.noreply.github.com>
2019-12-13 16:42:28 +0530

    Update basic python programm.py

--- basic python programm.py
+++ basic python programm.py
@@ -7,3 +7,26 @@ print(a*b)#multiplication
 print(a/b)#division quotient in float
 print(a//b)#floor division print the quotient integer
 print(a%b)#modulo operation show remainder alone 
+
+
+
+
+
+
+#basic add program using loop
+n=int(input())
+sum=0
+while(n>0):
+	sum+=n
+	n-=1
+print(sum)
+
+#basic add using for loop
+sum=0
+for i in range(1,10):
+	sum+=i
+print(sum)
+
+	
+
+
