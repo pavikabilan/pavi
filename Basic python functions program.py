863a46c94546ef54e83680a0811ee5781b7786ee
pavikabilan <47311025+pavikabilan@users.noreply.github.com>
2019-12-13 21:55:31 +0530

    Update Basic python functions program.py

--- Basic python functions program.py
+++ Basic python functions program.py
@@ -17,3 +17,18 @@ def add (a,d):
 	print(a%b)
 	
 add (a=10,b=5)	
+
+
+
+
+
+
+#add using function /user defined function 
+def add():
+	n=int(input())
+	sum=0
+	while(n>0):
+		sum+=n
+		n-=1
+	print(sum)
+add()	
