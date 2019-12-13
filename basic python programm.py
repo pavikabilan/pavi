2f1360edf52076121f8873b3ead04ecbfe4cf513
pavikabilan <47311025+pavikabilan@users.noreply.github.com>
2019-12-13 16:47:14 +0530

    Update basic python programm.py

--- basic python programm.py
+++ basic python programm.py
@@ -16,16 +16,16 @@ print(a%b)#modulo operation show remainder alone
 #basic add program using loop
 n=int(input())
 sum=0
-while(n>0):
-	sum+=n
-	n-=1
-print(sum)
+while(n>0): #check whether n is greater than 0 or not:
+	sum+=n   #sum = sum + n ( add previous sum and next n)
+	n-=1 # decrement n by 1
+print(sum) # print the final sum
 
 #basic add using for loop
 sum=0
-for i in range(1,10):
-	sum+=i
-print(sum)
+for i in range(1,10):   #  execute from 1 to 9
+	sum+=i # sum=sum+i 
+print(sum) #print the final sum 
 
 	
 
