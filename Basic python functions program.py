86c6f208f4f2f479d7455e9b8b466e19a5f13a92
pavikabilan <47311025+pavikabilan@users.noreply.github.com>
2019-12-13 17:01:56 +0530

    Update Basic python functions program.py

--- Basic python functions program.py
+++ Basic python functions program.py
@@ -3,3 +3,17 @@ def function ():  # function definition
 	print('pavithra')
 
 function()	#function call
+
+
+
+
+#basic arithmetic operation
+def add (a,d):
+	print(a+b)
+	print(a-b)
+	print(a*b)
+	print(a/b)
+	print(a//b)
+	print(a%b)
+	
+add (a=10,b=5)	
