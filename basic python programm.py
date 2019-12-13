958bda2b40ec16ea2713d8215760cc48ad30a48f
pavikabilan <47311025+pavikabilan@users.noreply.github.com>
2019-12-13 16:06:29 +0530

    Create basic python programm.py

---
+++ basic python programm.py
@@ -0,0 +1,9 @@
+#basic program
+a=9
+b=3
+print(a+b)#add
+print(a-b)#sub
+print(a*b)#multiplication
+print(a/b)#division quotient in float
+print(a//b)#floor division print the quotient integer
+print(a%b)#modulo operation show remainder alone 
