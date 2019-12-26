f73a1dcd6167f2b095be0b91479c76cec003e38d
pavithra <47311025+pavikabilan@users.noreply.github.com>
2019-12-26 17:29:51 +0530

    Create MATH.py

---
+++ MATH.py
@@ -0,0 +1,58 @@
+import math
+
+x = math.factorial(4)
+print(x)
+
+b = math.fabs(20.7/3.5)
+print(b)
+
+c = math.ceil(20.3)
+print(c)
+
+d = round(20.3)
+print(d) 
+
+f=math.pi
+print(f)
+
+e=math.e
+print(e)
+
+g=math.radians(60)
+print(g)
+
+h=math.degrees(40)
+print(h)
+
+x=math.sin(60)
+print(x)
+
+y=math.cos(50)
+print(y)
+
+z=math.tan(90)
+print(z)
+
+a=math.log(10)
+print(a)
+
+b=math.log10(10)
+print(b)
+
+c=math.exp(2)
+print(c)
+
+d=math.exp()
+
+
+
+
+
+
+
+
+
+
+
+
+
