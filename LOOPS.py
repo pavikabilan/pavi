797bf7029c24767569d1391b9f7843cf1ef26fb1
pavithra <47311025+pavikabilan@users.noreply.github.com>
2019-12-26 16:14:21 +0530

    Update LOOPS.py

--- LOOPS.py
+++ LOOPS.py
@@ -6,4 +6,16 @@ while a>0:
     rev=a%10
     d=(d*10)+rev
     a=a//10
-print(d)    
+print(d)
+
+
+# sum of digits
+
+a=int(input("enter the number:"))
+sum=0
+while a>0:
+    d=a%10
+    sum+=d
+    a=a//10
+print(sum)    
+
